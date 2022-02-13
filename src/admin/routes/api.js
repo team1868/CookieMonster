197527cd59e601fc658225ed05586bd751b4068c
cyandev <djs.bitcoin@gmail.com>
2022-02-13 23:13:59 +0000

    remove auth for admin page, keep it for setup

--- src/admin/routes/api.js
+++ src/admin/routes/api.js
@@ -13,52 +13,31 @@ router.use((req,res,next) => {
 })
 
 router.get("/auth", (req, res) => {
-    if (config.secrets.ACCESS_CODE === "") {
-        res.json({status: 2})
-    } else if (config.secrets.ACCESS_CODE == req.headers.authorization) {
-        res.json({status: 1})
-    } else {
-        res.json({status: 0})
-    }
+    res.json({status: 2})
 })
 
 router.get("/scouters", (req,res) => {
-    if (req.headers.authorization === config.secrets.ACCESS_CODE) {
-        res.json(ScoutingSync.getScouters())
-    } else {
-        res.json({error: "Not Authorized"})
-    }
+    res.json(ScoutingSync.getScouters())
 });
 
 router.get("/enterMatch", (req,res) => {
-    if (req.headers.authorization === config.secrets.ACCESS_CODE) {
-        for (let scouter of ScoutingSync.scouters) {
-            if (scouter.state.status == ScoutingSync.SCOUTER_STATUS.WAITING)
-                scouter.socket.emit("enterMatch");
-        }
-        res.json(true);
-    } else {
-        res.json({error: "Not Authorized"})
+    for (let scouter of ScoutingSync.scouters) {
+        if (scouter.state.status == ScoutingSync.SCOUTER_STATUS.WAITING)
+            scouter.socket.emit("enterMatch");
     }
+    res.json(true);
 })
+
 router.post("/setMatch", (req,res) => {
-    if (req.headers.authorization === config.secrets.ACCESS_CODE) {
-        ScoutingSync.setMatch(req.body);
-        ScoutingSync.assignScouters();
-        res.json(true);
-    } else {
-        res.json({error: "Not Authorized"})
-    }
+    ScoutingSync.setMatch(req.body);
+    ScoutingSync.assignScouters();
+    res.json(true);
 });
 
 router.get("/matches", async (req,res) => {
-    if (req.headers.authorization === config.secrets.ACCESS_CODE) {
-        res.json({
-            "allMatches": await ScoutingSync.getMatches(),
-            "currentMatch": ScoutingSync.match
-        })
-    } else {
-        res.json({error: "Not Authorized"})
-    }
+    res.json({
+        "allMatches": await ScoutingSync.getMatches(),
+        "currentMatch": ScoutingSync.match
+    })
 })
 module.exports = router;
